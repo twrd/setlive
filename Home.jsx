b3225e03fe3f4d3893577d9c3e197df23feec778
Badek <Przemyslaw.Badek@ts.fujitsu.com>
2016-12-08 17:47:33 +0100

    First commit of my crap.

---
+++ Home.jsx
@@ -0,0 +1,9 @@
+export class Home extends React.Component{
+	render(){
+		<div>
+			<h1>It is home</h1>
+			<b>{this.props.name}</b>
+		</div>
+	}
+	
+}
---
+++ app.jsx
@@ -0,0 +1,43 @@
+class Song extends React.Component{
+	render(){
+		return (
+		<div>
+			<h1>{this.props.song.title}</h1>
+			<hr/>
+			<ul>
+			{this.props.song.tags.map( (tag) => <li>{tag}</li> )}
+			</ul>
+			<hr/>
+			{this.props.song.lyrics}
+			<hr/>
+			{this.props.children}
+		</div>
+		)
+	}   
+	
+}
+
+
+
+class App extends React.Component {
+	
+	render() {
+		
+		var song = {
+			title: "Donna Summer - Hot stuff",
+			lyrics: "asdasdasd asdasd sa das dsa d",
+			tags: ["disco","kate"]
+		};
+		
+
+		return (
+			<div>
+				<Song song={song}/>
+			</div>
+		); 
+	}
+}
+
+
+const app = document.getElementById('app');
+ReactDOM.render(<App/>, app);
